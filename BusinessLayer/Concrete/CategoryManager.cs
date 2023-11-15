84a65f1f7b05e65efab0baaff1dadd5ef473086c
ufkkarayel <102018206+ufkkarayel@users.noreply.github.com>
2023-11-15 21:22:19 +0300

    Repository Added.

---
+++ BusinessLayer/Concrete/CategoryManager.cs
@@ -0,0 +1,20 @@
+﻿using DataAccessLayer.Concrete;
+using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Concrete
+{
+    public class CategoryManager 
+    {
+        Repository<Category> repo = new Repository<Category>();
+        public List<Category> GetAll()
+        {
+            return repo.List();
+        }
+        
+    }
+}
